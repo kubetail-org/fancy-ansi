794e8953e025d5b73f647fb5568965f4c417dfa9
Andres Morey <andresmarcel@gmail.com>
2024-05-10 15:28:32 +0300

    fixed linting issues

--- src/ansi-regex.ts
+++ src/ansi-regex.ts
@@ -1,10 +1,10 @@
 // Embedding this here because the ansi-regex import is
 // triggering an jspm error (https://github.com/kubetail-org/fancy-ansi/issues/16)
 // https://github.com/chalk/ansi-regex/blob/main/index.js
-export default function ansiRegex({onlyFirst = false} = {}) {
+export default function ansiRegex({ onlyFirst = false } = {}) {
   const pattern = [
     '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
-    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))'
+    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
   ].join('|');
 
   return new RegExp(pattern, onlyFirst ? undefined : 'g');
